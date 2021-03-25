cccab56cd706b92e77cf1ee99ec1a4ec7623496c
bigpel66 <bigpel66@icloud.com>
2021-03-25 16:41:08 +0900

    6.3 completed

--- color.h
+++ color.h
@@ -4,7 +4,8 @@
 # include "vec3.h"
 # include <iostream>
 
-void write_color(std::ostream &out, color pixel_color) {
+void write_color(std::ostream &out, color pixel_color)
+{
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
--- ray.h
+++ ray.h
@@ -3,19 +3,16 @@
 
 # include "vec3.h"
 
-class ray {
+class ray
+{
     public:
         ray() {}
-        ray(const point3& origin, const vec3& direction)
-            : orig(origin), dir(direction)
-        {}
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
         point3 origin() const  { return orig; }
         vec3 direction() const { return dir; }
 
-        point3 at(double t) const {
-            return orig + t*dir;
-        }
+        point3 at(double t) const { return orig + t * dir; }
 
     public:
         point3 orig;
