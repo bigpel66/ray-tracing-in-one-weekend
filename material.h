7573dbc687735f8257e221cbeb8fa0be14479df5
bigpel66 <bigpel66@icloud.com>
2021-03-27 19:56:09 +0900

    9.1 finish

---
+++ material.h
@@ -0,0 +1,14 @@
+#ifndef MATERIAL_H
+# define MATERIAL_H
+
+# include "rtweekend.h"
+
+struct hit_record;
+
+class material
+{
+	public:
+		virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
+};
+
+#endif
