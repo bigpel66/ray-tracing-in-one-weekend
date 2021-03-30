ca48167dfd64295023c741852d19f19a7c488502
BIGPEL <bigpel66@gmail.com>
2021-03-30 15:49:47 +0900

    finish of chapter 12

--- vec3.h
+++ vec3.h
@@ -139,7 +139,7 @@ inline vec3 random_in_unit_sphere()
 	{
 		auto p = vec3::random(-1, 1);
 		if (p.length_squared() >= 1)
-			continue;
+			continue ;
 		return (p);
 	}
 }
@@ -168,6 +168,17 @@ vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat)
 	vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
 	vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
 	return (r_out_perp + r_out_parallel);
+}
+
+vec3 random_in_unit_disk()
+{
+	while (true)
+	{
+		auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+		if (p.length_squared() >= 1)
+			continue ;
+		return (p);
+	}
 }
 
 #endif
