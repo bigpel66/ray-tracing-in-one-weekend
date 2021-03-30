cb868e39642421b8621a31d1ffc099e2228b03f6
bigpel66 <bigpel66@icloud.com>
2021-03-30 14:33:04 +0900

    finish of chapter 11, only 12 and final left

--- camera.h
+++ camera.h
@@ -6,22 +6,26 @@
 class camera
 {
 	public:
-		camera()
+		camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio)
 		{
-			auto aspect_ratio = 16.0 / 9.0;
-			auto viewport_height = 2.0;
-			auto viewport_width = viewport_height * aspect_ratio;
-			auto focal_length = 1.0;
+			auto theta = degrees_to_radians(vfov);
+			auto h = tan(theta / 2);
+			auto viewport_height = 2.0 * h;
+			auto viewport_width = aspect_ratio * viewport_height;
 
-			origin = point3(0, 0, 0);
-			horizontal = vec3(viewport_width, 0, 0);
-			vertical = vec3(0, viewport_height, 0);
-			lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+			auto w = unit_vector(lookfrom - lookat);
+			auto u = unit_vector(cross(vup, w));
+			auto v = cross(w, u);
+
+			origin = lookfrom;
+			horizontal = viewport_width * u;
+			vertical = viewport_height * v;
+			lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
 		}
 
-		ray get_ray(double u, double v) const
+		ray get_ray(double s, double t) const
 		{
-			return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
 		}
 
 	private:
@@ -32,4 +36,3 @@ class camera
 };
 
 #endif
-
--- sphere.h
+++ sphere.h
@@ -8,13 +8,14 @@ class sphere : public hittable
 {
 	public:
 		sphere() {}
-		sphere(point3 cen, double r) : center(cen), radius(r) {}
+		sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {}
 		virtual ~sphere() {}
 
 		virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
 	public:
 		point3 center;
 		double radius;
+		shared_ptr<material> mat_ptr;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
@@ -42,6 +43,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 	rec.p = r.at(rec.t);
 	vec3 outward_normal = (rec.p - center) / radius;
 	rec.set_face_normal(r, outward_normal);
+	rec.mat_ptr = mat_ptr;
 
 	return (true);
 }
